9b3168a1bd08186a09ed9613b16a04bdc11e183e
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-24 15:02:41 -0300

    feat: using references and finish modules

---
+++ exe2/index.js
@@ -0,0 +1,5 @@
+import People from "./people";
+
+const People = new People("John Doe");
+console.log(People.toString()); // Output: Name: John Doe
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,30 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 
 module.exports = {
-  entry: './ex/index.js',
-  output: {
-    path: __dirname + '/public',
-    filename: './bundle.js'
-  },
-  devServer: {
-    port: 8080,
-    contentBase: './public'
-  },plugins:[
-    new ExtractTextPlugin('app.css')
-  ],
-  module:{
-    loaders: [{
-      test: /.js?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: {
-        presets: ['es2015', 'react'],
-        plugins: ['transform-object-rest-spread']
-      }
-    },{
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract("style-loader","css-loader")
-    }]
-  }
+    entry: './exe/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ],
+    module: {
+        loaders: [{
+            test: /.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        }]
+    }
 }
